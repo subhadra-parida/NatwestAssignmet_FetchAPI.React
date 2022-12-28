da4e4653198fa5e1e8da0dcc0143b7c61d4a5ced
subhadra-parida <99946995+subhadra-parida@users.noreply.github.com>
2022-12-28 22:41:19 +0530

    FaitchAPI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import React,{ useState , useEffect } from "react";
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default function App(){
+  const [data,setData]  = useState([])
+  const getData=async()=>{
+    const Response = await fetch(`https://jsonplaceholder.typicode.com/users`)
+    const Result = await Response.json();
+    setData(Result)
+    console.log(data)
+  }
+  useEffect(()=>{
+    getData()
+  },[])
+  return(
+    <div>
+      <table>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>UserName</th>
+          <th>Email</th>
+          <th>City.address</th>
+          <th>Zipcode</th>
+          <th>Phone</th>
+          <th>CompanyName</th>
+        </tr>
+        </table>
+        {
+          data.map((item,index)=>{
+            return(
+              <table>
+              <tr>
+                <td>{item.id}</td>
+                <td>{item.name}</td>
+                <td>{item.username}</td>
+                <td>{item.email}</td>
+                <td>{item.address.city}</td>
+                <td>{item.address.zipcode}</td>
+                <td>{item.phone}</td>
+                <td>{item.company.name}</td>
+              </tr>
+              </table>
+            )
+          })
+        }
+      
     </div>
-  );
+  )
 }
 
-export default App;
